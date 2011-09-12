2ed8149f3a69394b2d6524c97794fb358de2cdfc
pierre@chalamet.net <pierre@chalamet.net@49b2f017-7fb6-9c46-f23a-921cbc76fcdd>
2011-09-12 21:36:08 +0000

    zip creation after build all

--- GenerateVersion.bat
+++ GenerateVersion.bat
@@ -4,8 +4,9 @@ setlocal
 set VERSION=%1
 if "%VERSION%" == "" goto usage
 
-msbuild /t:GenerateVersion /p:Version=%VERSION% /p:Configuration=Debug cassandra-sharp.targets
-msbuild /t:GenerateVersion /p:Version=%VERSION% /p:Configuration=Release cassandra-sharp.targets
+msbuild /t:GenerateVersion /p:Version=%VERSION% /p:Configuration=Debug cassandra-sharp.targets || exit /b 5
+msbuild /t:GenerateVersion /p:Version=%VERSION% /p:Configuration=Release cassandra-sharp.targets || exit /b 5
+msbuild /t:ZipBinaries /p:Version=%VERSION% cassandra-sharp.targets || exit /b 5
 
 :done
 endlocal
