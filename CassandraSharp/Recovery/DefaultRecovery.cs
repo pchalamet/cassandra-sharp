3e507baa11128dddaa446850e9261ec778ba894a
pierre@chalamet.net <pierre@chalamet.net@49b2f017-7fb6-9c46-f23a-921cbc76fcdd>
2012-03-14 22:35:04 +0000

    cleanup

--- CassandraSharp/Recovery/DefaultRecovery.cs
+++ CassandraSharp/Recovery/DefaultRecovery.cs
@@ -1,34 +1,22 @@
-﻿// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// 
-// http://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  See the License for the specific language governing permissions and
-// limitations under the License.
-namespace CassandraSharp.Recovery
+﻿namespace CassandraSharp.Recovery
 {
     using System.Collections.Generic;
-    using System.Runtime.CompilerServices;
     using System.Timers;
     using CassandraSharp.EndpointStrategy;
     using CassandraSharp.Transport;
     using CassandraSharp.Utils;
-    using log4net;
     using Thrift.Transport;
+    using log4net;
 
     internal class DefaultRecovery : IRecoveryService
     {
+        private readonly object _lock;
+
         private readonly ILog _log = LogManager.GetLogger(typeof(DefaultRecovery));
 
         private readonly Timer _timer;
 
         private readonly List<RecoveryItem> _toRecover;
-
-        private readonly object _lock;
 
         public DefaultRecovery()
         {
