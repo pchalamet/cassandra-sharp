c222c17d2188341a6214a7c670e1ad2385a54241
Pierre Chalamet <pierre@chalamet.net>
2013-02-13 09:07:38 +0100

    fixed nuget publishing

--- PublishNuGetVersion.cmd
+++ PublishNuGetVersion.cmd
@@ -4,7 +4,7 @@ setlocal
 set VERSION=%1
 if "%VERSION%" == "" goto usage
 
-ThirdParties\NuGet\NuGet.exe push OutDir\cassandra-sharp.%VERSION%.nupkg
+.nuget\NuGet.exe push OutDir\cassandra-sharp.%VERSION%.nupkg
 
 :done
 endlocal
