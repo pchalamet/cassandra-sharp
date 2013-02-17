e2eccc230b7a96f0c69479024764f20587a1550a
Pierre Chalamet <pierre@chalamet.net>
2013-02-17 14:55:05 +0100

    new test

---
+++ CassandraSharpUnitTests/Performance/PacketSizePerformanceTest.cs
@@ -0,0 +1,110 @@
+﻿// cassandra-sharp - a .NET client for Apache Cassandra
+// Copyright (c) 2011-2013 Pierre Chalamet
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace CassandraSharpUnitTests.Performance
+{
+    using System;
+    using System.Diagnostics;
+    using CassandraSharp;
+    using CassandraSharp.CQL;
+    using CassandraSharp.CQLPoco;
+    using CassandraSharp.Config;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class PacketSizePerformanceTest
+    {
+        private static void InsertData(string data, IPreparedQuery<NonQuery> preparedQuery)
+        {
+            Console.WriteLine("Buffer size {0}", data.Length);
+            Stopwatch totalwatch = Stopwatch.StartNew();
+            for (var i = 0; i < 200; ++i)
+            {
+                Stopwatch stopwatch = Stopwatch.StartNew();
+                preparedQuery.Execute(new {x = "abc", y = data}).AsFuture().Wait();
+                stopwatch.Stop();
+                Console.WriteLine("Insert: {0}", stopwatch.ElapsedMilliseconds);
+            }
+            totalwatch.Stop();
+            Console.WriteLine("Total inserts: {0}", totalwatch.ElapsedMilliseconds);
+        }
+
+        [Test]
+        public void PacketSizeTest()
+        {
+            //run Write Performance Test using cassandra-sharp driver
+            CassandraSharpConfig cassandraSharpConfig = new CassandraSharpConfig();
+            ClusterManager.Configure(cassandraSharpConfig);
+
+            ClusterConfig clusterConfig = new ClusterConfig
+                {
+                        Endpoints = new EndpointsConfig
+                            {
+                                    Servers = new[] {"localhost"}
+                            },
+                };
+
+            using (ICluster cluster = ClusterManager.GetCluster(clusterConfig))
+            {
+                ICqlCommand cmd = new PocoCommand(cluster);
+
+                const string dropFoo = "drop keyspace Tests";
+                try
+                {
+                    cmd.Execute(dropFoo).AsFuture().Wait();
+                }
+                catch
+                {
+                }
+
+                const string createFoo = "CREATE KEYSPACE Tests WITH replication = {'class': 'SimpleStrategy', 'replication_factor' : 1}";
+                Console.WriteLine("============================================================");
+                Console.WriteLine(createFoo);
+                Console.WriteLine("============================================================");
+
+                var resCount = cmd.Execute(createFoo);
+                resCount.AsFuture().Wait();
+                Console.WriteLine();
+                Console.WriteLine();
+
+                const string createBar = "CREATE TABLE Tests.tbl ( x varchar primary key, y varchar )";
+                Console.WriteLine("============================================================");
+                Console.WriteLine(createBar);
+                Console.WriteLine("============================================================");
+                resCount = cmd.Execute(createBar);
+                resCount.AsFuture().Wait();
+                Console.WriteLine();
+                Console.WriteLine();
+
+                var preparedQuery = cmd.Prepare("insert into Tests.tbl (x, y) values (?, ?)");
+
+                InsertData(new string('x', 1423), preparedQuery);
+                Console.WriteLine();
+
+                InsertData(new string('x', 1424), preparedQuery);
+                Console.WriteLine();
+
+                Console.WriteLine("============================================================");
+                Console.WriteLine(dropFoo);
+                Console.WriteLine("============================================================");
+
+                resCount = cmd.Execute(dropFoo);
+                resCount.AsFuture().Wait();
+            }
+
+            ClusterManager.Shutdown();
+        }
+    }
+}
