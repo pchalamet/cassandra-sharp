01934bd925143ad1f4d7684a8e1bcdd8b2e784e6
Pierre Chalamet <pierre@chalamet.net>
2013-03-12 22:33:52 +0100

    fixed publish

--- Publish.cmd
+++ Publish.cmd
@@ -4,7 +4,7 @@ setlocal
 set /p PUBLISH=Publish to NuGet ? [Y/n]
 if "%PUBLISH%" NEQ "Y" goto :done
 
-msbuild /t:Publish cassandra-sharp-nuget.targets || goto :done
+msbuild /t:Publish cassandra-sharp.targets || goto :done
 
 :done
 endlocal
