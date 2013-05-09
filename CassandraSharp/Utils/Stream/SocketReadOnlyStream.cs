5ddd01a24a10be97feae81af784f53852968a839
Pierre Chalamet <pierre@chalamet.net>
2013-05-09 10:45:07 +0200

    Poll socket before considering disconnected

--- CassandraSharp/Utils/Stream/SocketReadOnlyStream.cs
+++ CassandraSharp/Utils/Stream/SocketReadOnlyStream.cs
@@ -111,8 +111,8 @@ namespace CassandraSharp.Utils.Stream
             {
                 int tmpRead = socket.Receive(buffer, offset + read, len - read, SocketFlags.None);
 
-                // always consider 0 as a peer graceful disconnection
-                if (0 == tmpRead)
+                // consider 0 as a peer graceful disconnection
+                if (0 == tmpRead && socket.Poll(1, SelectMode.SelectRead))
                 {
                     return read;
                 }
